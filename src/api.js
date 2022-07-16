4a884f86b0899dc0a666f8d214ddf512bce873fc
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-07-16 08:53:29 +0700

    Install package and build city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '14b871c6f9mshf397cf3678ffdacp134aefjsnaa0102b031d0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
